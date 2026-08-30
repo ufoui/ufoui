ad243a2dd873af7321c59dd11350687ee13dc984
kgneteu <77528950+kgneteu@users.noreply.github.com>
2026-08-30 18:13:44 +0200

    chips

--- packages/ufoui/src/components/chip/chip.tsx
+++ packages/ufoui/src/components/chip/chip.tsx
@@ -1,49 +1,50 @@
-import { forwardRef } from 'react';
+import { forwardRef, ReactElement } from 'react';
 
 import { ElementFont, ElementSize } from '../../utils';
-import { ButtonBase, ButtonBaseProps } from '../base/buttonBase';
+import { ButtonBase, ButtonBaseProps } from '../base';
 
 export type ChipType = 'assist' | 'filter' | 'input' | 'suggestion';
 
-export interface ChipProps
-  extends Omit<ButtonBaseProps, 'elementClass' | 'iconClass' | 'labelClass'> {
-  // icon: ReactElement;
-  /** Defines the visual style and behavior of the chip. @default 'assist' */
-  chipType?: ChipType;
+export interface ChipProps extends Omit<ButtonBaseProps, 'elementClass' | 'iconClass' | 'labelClass' | 'type'> {
+    /** Avatar placed in the leading slot. */
+    avatar?: ReactElement;
+    /** Defines the visual style and behavior of the chip. @default 'assist' */
+    type?: ChipType;
 }
 
 export const Chip = forwardRef<HTMLButtonElement, ChipProps>(
-  (
-    {
-      font,
-      size = 'small',
-      shape = 'smooth',
-      selectedShape = 'smooth',
-      borderColor = 'outlineVariant',
-      ...props
-    }: ChipProps,
-    ref,
-  ) => {
-    const fontMap: Record<ElementSize, ElementFont> = {
-      extraSmall: 'labelLarge',
-      small: 'labelLarge',
-      medium: 'titleMedium',
-      large: 'headlineSmall',
-      extraLarge: 'headlineLarge',
-    };
-    return (
-      <ButtonBase
-        borderColor={borderColor}
-        font={font ?? fontMap[size]}
-        ref={ref}
-        selectedShape={selectedShape}
-        {...props}
-        elementClass="uui-chip"
-        shape={shape}
-        size={size}
-      />
-    );
-  },
+    (
+        {
+            font,
+            size = 'small',
+            shape = 'smooth',
+            selectedShape = 'smooth',
+            borderColor = 'outlineVariant',
+            type,
+            ...props
+        }: ChipProps,
+        ref
+    ) => {
+        const fontMap: Record<ElementSize, ElementFont> = {
+            extraSmall: 'labelLarge',
+            small: 'labelLarge',
+            medium: 'titleMedium',
+            large: 'headlineSmall',
+            extraLarge: 'headlineLarge',
+        };
+        return (
+            <ButtonBase
+                borderColor={borderColor}
+                font={font ?? fontMap[size]}
+                ref={ref}
+                selectedShape={selectedShape}
+                {...props}
+                elementClass="uui-chip"
+                shape={shape}
+                size={size}
+            />
+        );
+    }
 );
 
 Chip.displayName = 'Chip';
